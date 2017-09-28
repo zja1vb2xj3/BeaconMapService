34cbe78d43a3f5e5aa7e697a1ad0cda68007238e
PDG <zja1vb2xj3@naver.com>
2017-09-29 01:55:46 +0900

    소스코드 정리

--- app/src/main/java/com/example/user/beconmapservice/Activity/MainActivity.java
+++ app/src/main/java/com/example/user/beconmapservice/Activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
